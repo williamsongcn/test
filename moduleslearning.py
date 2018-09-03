d403385ef21795e99b30c66455648208bec13686
williamsongcn@gmail.com <williamsongcn@gmail.com>
2018-09-03 20:22:21 +0800

    modules note

---
+++ moduleslearning.py
@@ -0,0 +1,15 @@
+import numpy as np 
+import pandas as  pd 
+import matplotlib as plt
+np.random.seed()
+# 一个seed值对应一个随机数，设置一下后失效
+np.random.random()
+#产生一个随机数
+np.linspace(start,stop,sequenceSize=50)
+#产生一个等差数列
+np.random.normal(from prml.features import PolynomialFeatures
+from prml.linear import (
+    LinearRegressor,
+    RidgeRegressor,
+    BayesianRegressor
+))
